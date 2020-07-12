1ff9ae85e209890abcdd36b9d798a1a9cef12fd5
Megha Shrestha <meghashrestha30@gmail.com>
2020-07-12 21:10:01 +0545

    first commit

---
+++ a.py
@@ -0,0 +1,21 @@
+# A. Make pythonic solutions for each of the following data structure
+# and algorithm problems.
+# a) Bubble Sort
+
+def bubblesort(list):
+    for i in range(len(list)):
+        for j in range(len(list)-1):
+            if list[j] > list[j+1] :
+                list[j] , list[j+1] = list[j+1] , list[j]
+            else:
+                pass
+    return list
+list = []
+n = int(input(" enter the number of item in the list : "))
+for i in range(0, n):
+    element = int(input("enter number: "))
+    list.append(element)
+print("unsorted List")
+print(list)
+print("sorted list")
+print(bubblesort(list))
---
+++ try.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+print("I'm going to sleep now.")
+
+sleep(3)
+
+print("I woke up!")
